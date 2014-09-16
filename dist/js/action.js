6fdaaeff29052bd24fa9f8247a44c03652e2bb8e
stevenxzw <stevenxzw@gmail.com>
2014-09-16 18:43:04 +0800

    autoInputTest

--- dist/js/action.js
+++ dist/js/action.js
@@ -49,8 +49,13 @@
              uri : '',
              input : '',
              delay : 500,
+             choosedMax : 1,
+             floatCs : 'chooseFloat',
+             choosedCs : 'choosed',
              timer : null,
-             temp : '<span rel="{.id}">{.text}</span>'
+             choosed : '<span id="{.id}">{.text} <span class="del">X</span></span>',
+             itemTemp : '<li rel="{.id}"><a href="#">{.text}</a></li>',
+             floatTemp : '<ul class="chooseFloat">{.lis}</ul>'
          }, opt);
          this.init();
     }
@@ -64,23 +69,63 @@
 
             var that = this;
             this.input = this.input || this.view;
-            this.couser = new cursorControl(this.input[0]);
-            $(this.input).on('keyup', function(e){
-                //console.log(this.selectionStart);
+            //this.couser = new cursorControl(this.input[0]);
+            $(this.input).on('keyup click', function(e){
                 clearTimeout(that.timer);
                 that.timer = setTimeout(function(){that.get.call(that,e)}, that.delay);
+            });
+            $('body').on('click', function(e){
+                if($(e.target).closest(that.view).length === 0 || $(e.target).has(that.input).length){
+                    that.removeFloat();
+                }
+            });
+            this.view.on('click', '.'+this.floatCs+' li', function(e){
+                e.preventDefault();
+                //console.log($(this).text());
+                that.afterChoosed($(this));
+                that.removeFloat();
+            });
+
+            this.view.on('click', '.'+this.choosedCs+' .del', function(e){
+                $(this).parent().remove();
             })
         },
 
+        afterChoosed : function(dom){
+            var text = dom.text(), id = dom.attr('rel');
+            this.createChoosed(id, text);
+            this.clearInput('');
+        },
+
+        clearInput : function(v){
+            this.setVal(v);
+        },
+
+        removeFloat : function(){
+            this.view.find('.'+this.floatCs).remove();
+        },
+
         getVal : function(){
-          return this.input.val();
+            if(this.input.attr('type') === 'text' || this.input.attr('type') === 'textarea')
+                return $.trim(this.input.val());
+            else
+                return $.trim(this.input.text());
+        },
+
+        setVal : function(v){
+            if(this.input.attr('type') === 'text' || this.input.attr('type') === 'textarea')
+                this.input.val(v);
+            else
+                this.input.text(v);
         },
 
         get : function(e){
-            console.log(this.getVal());
+            var v = this.getVal();
+            console.log(v);
             this.input.focus();
-            console.log(this.couser.getStart());
-            //debugger;
+            //console.log(this.couser.getStart());
+            if(v)
+                this.getByAjax(v);
         },
 
         getByAjax : function(str){
@@ -90,9 +135,40 @@
             this.reader(data);
         },
 
-        reader : function(){
-            
+        reader : function(data){
+            var lis = [], that = this;
+            $.each(data, function(i, item){
+                lis.push(that.parse(that.itemTemp, item));
+            });
+            var ul = that.parse(that.floatTemp, {lis:lis.join('')});
+            this.removeFloat();
+            this.view.append(ul);
+            this.view.find('.'+this.floatCs).css({'top':this.input.offset().top-15});
+            console.log(ul);
+        },
 
+        createChoosed : function(id, text){
+            var cd = this.view.find('.'+this.choosedCs);
+            if(this.choosedMax === 1){
+                cd.html(this.parse(this.choosed, {id:id, text:text})).show();
+            }else if(cd.children().length < this.choosedMax){
+                cd.append(this.parse(this.choosed, {id:id, text:text})).show();
+            }
+        },
+
+        parse : function(htmls, map){
+            var tplReg =  /\{(\.?[\w_|$]+)(\.[\w_$]+)?\}/g;
+            return htmls.replace(tplReg, function(s, k , k1){
+                var v, modfs, k_str, key;
+
+                if (k.charCodeAt(0) === 46)  {
+                    k_str = k.substr(1);
+                    modfs = k_str.split('|');
+                    key = modfs.shift();
+                    v = map[key];
+                }
+                return v;
+            });
         }
 
     }
@@ -100,7 +176,7 @@
     $(function(){
 
         console.log('init');
-        new AutoInput({view : $('#ableDiv')});
+        new AutoInput({view : $('.inputPanel').eq(0), input : $('#ableDiv')});
 
     })
 
